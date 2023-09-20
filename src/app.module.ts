c56f41c928d588655f7cb960986cd8ffa3e0582e
magnojunior07 <magnojunior319@gmail.com>
2023-09-19 20:32:04 -0400

    fix: solve some conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './modules/user/user.module';
-import { PostModule } from './modules/post/post.module';
-
-@Module({
-  imports: [UserModule, PostModule],
-  controllers: [AppController],
-  providers: [AppService],
-=======
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -17,11 +5,11 @@ import { UserModule } from "./modules/user/user.module";
 import { ProfileModule } from "./modules/profile/profile.module";
 import { AddressModule } from "./modules/address/address.module";
 import { AuthModule } from "./modules/auth/auth.module";
+import { PostModule } from "./modules/post/post.module";
 
 @Module({
-    imports: [UserModule, ProfileModule, AddressModule, AuthModule],
+    imports: [UserModule, ProfileModule, AddressModule, AuthModule, PostModule],
     controllers: [AppController],
     providers: [AppService],
->>>>>>> 10c9df196a4e30a55607aa723441681ac580459d
 })
 export class AppModule {}
